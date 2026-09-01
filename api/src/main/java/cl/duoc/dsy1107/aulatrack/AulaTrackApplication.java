d3d221c9d7493802eea99348de3bfa01d315ddf5
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2026-08-31 20:37:58 -0400

    feat(api): add Spring Boot application

---
+++ api/src/main/java/cl/duoc/dsy1107/aulatrack/AulaTrackApplication.java
@@ -0,0 +1,11 @@
+package cl.duoc.dsy1107.aulatrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaTrackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AulaTrackApplication.class, args);
+    }
+}
