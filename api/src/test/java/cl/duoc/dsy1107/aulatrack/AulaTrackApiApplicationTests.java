71234156e14a0c715db0c34d5d592efb85fb3eb7
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2026-09-01 03:23:14 -0400

    Add backend context smoke test

---
+++ api/src/test/java/cl/duoc/dsy1107/aulatrack/AulaTrackApiApplicationTests.java
@@ -0,0 +1,12 @@
+package cl.duoc.dsy1107.aulatrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaTrackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
